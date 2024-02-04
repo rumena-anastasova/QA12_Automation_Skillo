39cf8867aa4615d16fce9cf8daee00a620849e61
rumena-anastasova <147644709+rumena-anastasova@users.noreply.github.com>
2024-02-04 16:11:35 +0200

    Create new java classes

---
+++ src/W02_Exercises_01/Second.java
@@ -0,0 +1,12 @@
+package W02_Exercises_01;
+
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello2");
+
+    }
+}
